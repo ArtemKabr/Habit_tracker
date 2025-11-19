abfa7eeb528c59531513dc29ac28a5572a931497
Artem Kabritskii <artemkabr7@gmail.com>
2025-11-19 11:14:26 +0300

    "Реализована JWT-аутентификация в проекте. Что сделано: - Установлена библиотека djangorestframework-simplejwt через Poetry. - Настроен класс аутентификации DRF на использование JWT токенов. - Добавлены настройки SimpleJWT (время жизни access и refresh токенов). - Добавлены эндпоинты для получения и обновления токенов: /token/ и /token/refresh/. - Настроены маршруты users для работы с регистрацией, профилем и JWT. - Выполнена проверка работы JWT через Postman, получены корректные access и refresh токены. Зачем сделано: - Обеспечивает безопасную авторизацию пользователей. - Подготавливает архитектуру проекта к созданию CRUD API привычек, Telegram-уведомлений и Celery-задач. Этап полностью завершён. Проект готов к реализации моделей Habit и валидаторов."

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -25,10 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@=v+^&+qee5_+d(b2&@c)q4)yf1%c7stafn!ef$ecbgizqha6m"
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -112,7 +115,14 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ]
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 
@@ -143,7 +153,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG") == "True"
+
 
 
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 # users/urls.py — маршруты для регистрации и профиля
 
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("profile/", ProfileView.as_view(), name="profile"),
+# JWT маршруты
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
